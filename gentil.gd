a42f54febd4e192390647a03f3fae718f5056049
cyffux <65236167+cyffux@users.noreply.github.com>
2020-10-09 18:29:23 +0200

    jump + network

--- gentil.gd
+++ gentil.gd
@@ -1,18 +1,39 @@
-extends Node2D
+extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var speed = 250
+var jump = 0
+var jump_state = 2.0
+var jump_change = 0
+var jump_number = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$KinematicBody2D/Sprite/AnimationPlayer.play("idle")
+	$Sprite/AnimationPlayer.play("idle")
 
 
 func _process(delta):
+	var velocity=Vector2()
 	if Input.is_action_pressed("ui_left"):
-		position.x-=7
-	elif Input.is_action_pressed("ui_right"):
-		position.x+=7
+		velocity.x-=1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x+=1
+	if Input.is_action_just_pressed("jump"):
+		if jump_number>0:
+			jump = 40
+			jump_state = 2
+			jump_change = 0
+			jump_number -= 1
+	if jump > 0:
+		jump-=1
+		jump_change+=1
+		if jump_change == 10:
+			jump_change = 0
+			jump_state -= 0.5
+		velocity.y-=jump_state
+	velocity.y+=1
+	var move = move_and_slide(velocity*speed)
+	if move.y == 0 and jump == 0:
+		jump_number=3
+
+
---
+++ multi.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var serv = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if serv == 1:
+		var peer = NetworkedMultiplayerENet.new()
+		peer.create_server(1, 2)
+		get_tree().network_peer = peer
+	else:
+		var peer = NetworkedMultiplayerENet.new()
+		peer.create_client("192.168.1.138", 1)
+		get_tree().network_peer = peer
+
+func network_peer_connected(id):
+	print(id)
