bf17a265d764a28c64b726d19f1f27f38e32adf0
cyffux <65236167+cyffux@users.noreply.github.com>
2020-10-09 20:12:01 +0200

    update multi

--- multi.gd
+++ multi.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var serv = 1
+var position = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- méchant.gd
+++ méchant.gd
@@ -1,38 +1,9 @@
 extends KinematicBody2D
 
-
-var speed = 250
-var jump = 0
-var jump_state = 2.0
-var jump_change = 0
-var jump_number = 3
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite/AnimationPlayer.play("idle")
 
 
 func _process(delta):
-	var velocity=Vector2()
-	if Input.is_action_pressed("right2"):
-		velocity.x-=1
-	if Input.is_action_pressed("left2"):
-		velocity.x+=1
-	if Input.is_action_just_pressed("jump2"):
-		if jump_number>0:
-			jump = 40
-			jump_state = 2
-			jump_change = 0
-			jump_number -= 1
-	if jump > 0:
-		jump-=1
-		jump_change+=1
-		if jump_change == 10:
-			jump_change = 0
-			jump_state -= 0.5
-		velocity.y-=jump_state
-	velocity.y+=1
-	var move = move_and_slide(velocity*speed)
-	if move.y == 0 and jump == 0:
-		jump_number=3
+	position = get_parent().get_parent().pos
 
