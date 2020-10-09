0f594a0172cb09e9196b6b02cd8133442fc4171f
cyffux <65236167+cyffux@users.noreply.github.com>
2020-10-09 20:36:26 +0200

    Update multi.gd

--- multi.gd
+++ multi.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var serv = 1
+var playerid=0
 
 # Called when the node enters the scene tree for the first time.
 
@@ -21,9 +22,15 @@ func _ready():
 		var peer = NetworkedMultiplayerENet.new()
 		peer.create_client("192.168.1.138", 31400)
 		get_tree().network_peer = peer
+	set_network_master(1)
 
 func connect_to_server(id):
+	print(str(id)+" is connected")
 	print(id)
+
+func _player_connected(id):
+	print(str(id)+" is connected")
+	var playerid=id
 
 func _connected_ok():
 	print("connected")
@@ -32,4 +39,8 @@ func _connected_fail():
 	print("fail")
 
 func _process(delta):
-	rpc("position", get_parent().pos, get_tree().get_network_unique_id())
+	if is_network_master():
+		rset("position",3)
+	else:
+		rset("position",2)
+	rpc("position", Vector2(66,66), get_tree().get_network_unique_id())
