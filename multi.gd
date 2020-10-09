97ece189fef4890d138a10ed559b8bb11f0cc9b7
cyffux <65236167+cyffux@users.noreply.github.com>
2020-10-09 18:49:30 +0200

    maj multi

--- multi.gd
+++ multi.gd
@@ -1,11 +1,14 @@
 extends Node
 
-var serv = 1
+var serv = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_ok")
+	get_tree().connect("connection_failed", self, "_connected_fail")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
 	if serv == 1:
 		var peer = NetworkedMultiplayerENet.new()
 		peer.create_server(1, 2)
@@ -18,3 +21,6 @@ func _ready():
 
 func _player_connected():
 	print("a")
+
+func _connected_ok():
+	print("connected")
